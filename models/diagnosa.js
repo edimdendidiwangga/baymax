ee9f0e142d4ecfc2d552a42d3b58ceee70da9e64
ridho0 <ridho230994@gmail.com>
2017-04-20 20:27:05 +0700

    bikin check point

--- models/diagnosa.js
+++ models/diagnosa.js
@@ -7,8 +7,24 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        Diagnosa.belongsTo(models.Penyakit,{foreignKey: 'penyakit_id'})
       }
     }
   });
   return Diagnosa;
 };
+
+// var User = this.sequelize.define('user', {/* attributes */})
+//   , Company  = this.sequelize.define('company', {/* attributes */});
+//
+// User.belongsTo(Company); // Will add companyId to user
+//
+// var User = this.sequelize.define('user', {/* attributes */}, {underscored: true})
+//   , Company  = this.sequelize.define('company', {
+//     uuid: {
+//       type: Sequelize.UUID,
+//       primaryKey: true
+//     }
+//   });
+//
+// User.belongsTo(Company); // Will add company_uuid to user
