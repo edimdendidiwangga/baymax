80ff27a7f89c061a2be3945faea3243843d69b9c
edimdendidiwangga <edimdendy@gmail.com>
2017-04-21 11:21:27 +0700

    login

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var model = require('../models')
+const crypto = require('crypto');
+const shortid = require('shortid');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users/index', { users : [] });
-});
 
-router.get('/register', function(req, res, next) {
-  res.render('users/register', {  });
-});
-router.post('/register', function(req, res, next) {
-  model.Pasien.create({
-    name: req.body.name,
-    email: req.body.email,
-    username: req.body.username,
-    password: req.body.password
-  }).then((data)=>{
-    res.redirect('/')
-  })
-});
+
 router.get('/searchDisease', function(req, res, next) {
-  model.Penyakit.findAll()
-  .then((penyakits)=>{
-    let item = []
-    penyakits.forEach((penyakit)=>{
-      item.push(penyakit.name)
+  if (req.session.username) {
+    model.Penyakit.findAll()
+    .then((penyakits)=>{
+      let item = []
+      penyakits.forEach((penyakit)=>{
+        item.push(penyakit.name)
+      })
+      //res.send(item)
+      res.render('users/search_disease', { data : item });
     })
-    //res.send(item)
-    res.render('users/search_disease', { data : item });
-  })
-
+  } else {
+      res.render('auth/login')
+  }
 });
 router.post('/searchDisease', function(req, res, next) {
   let search_disease = req.body.disease;
@@ -47,15 +37,21 @@ router.post('/searchDisease', function(req, res, next) {
 
 })
 router.get('/searchDiagnosis', function(req, res, next) {
-  model.Diagnosa.findAll()
-  .then((diagnosas)=>{
-    let item = []
-    diagnosas.forEach((diagnosa)=>{
-      item.push(diagnosa.gejala)
+  if (req.session.username) {
+    model.Diagnosa.findAll()
+    .then((diagnosas)=>{
+      let item = []
+      diagnosas.forEach((diagnosa)=>{
+        item.push(diagnosa.gejala)
+      })
+      //res.send(item)
+      res.render('users/search_diagnosis', { data : item });
     })
-    //res.send(item)
-    res.render('users/search_diagnosis', { data : item });
-  })
+  } else {
+      res.render('auth/login')
+  }
+
+
 });
 
 router.post('/searchDiagnosis', function(req, res, next) {
@@ -72,4 +68,47 @@ let search_gejala = req.body.diagnosis;
 })
 });
 
+router.post('/register', function(req, res, next){
+  console.log(req.body);
+  const secret = shortid.generate();
+  const hash = crypto.createHmac('sha256', secret)
+                     .update(req.body.password)
+                     .digest('hex');
+    model.Pasien.create({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: hash,
+      salt: secret,
+      role: 'client'
+   }).then((user) => {
+     res.redirect('/')
+   })
+})
+
+router.post('/login', function(req, res, next){
+  model.Pasien.find({
+    where: {
+      email: req.body.email
+    }
+  }).then((user) => {
+    const hash = crypto.createHmac('sha256', user.salt)
+                       .update(req.body.password)
+                       .digest('hex');
+    if (user.password == hash) {
+      req.session.name = user.name
+      req.session.username = user.username
+      req.session.id = user.id
+      req.session.email = user.email
+      req.session.role = user.role
+
+      console.log('Authentication success');
+      res.redirect('/')
+    } else{
+      console.log('Authentication fail');
+      res.redirect('/login')
+
+    }
+  })
+})
 module.exports = router;
